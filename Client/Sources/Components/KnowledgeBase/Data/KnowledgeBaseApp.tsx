f10ddb41be34e2ae345b994576ce7bd4ce5ae417
alevnyacow <alevnyacow@gmail.com>
2021-04-24 22:39:04 +0300

    client application prototype

---
+++ Client/Sources/Components/KnowledgeBase/Data/KnowledgeBaseApp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const KnowledgeBaseApp: React.FC<Props> = ({}) => {
+    return <h1>Knowledge base app</h1>;
+};
---
+++ Client/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./Sources/index.tsx",
+    output: {
+        path: path.join(__dirname, "/Transpiled"),
+        filename: "react_spa_bundle.js"
+    },
+    devtool: "inline-source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        plugins: [new TsconfigPathsPlugin()]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.join(__dirname, "index.html"),
+        filename: "index.html"
+    })],
+    module: {
+        rules: [
+            { test: /.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    devServer: {
+        contentBase: ["Transpiled"]
+    }
+};
